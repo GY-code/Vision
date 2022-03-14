6097cf51f9b2b57ac681151f8a96a30c99fe83b3
GY-code <gu_yi163@163.com>
2022-03-14 18:48:42 +0800

    drawable structure with vector completed devices list and more detailed multi-device drawing needed.

--- app/src/main/java/t20220049/sw_vision/Connection.java
+++ app/src/main/java/t20220049/sw_vision/Connection.java
@@ -1,64 +1,93 @@
 package t20220049.sw_vision;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.widget.Button;
-
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 
-/**
- * Created by Wangrx on 2017/11/2.
- */
+import androidx.annotation.Nullable;
+
 
 public class Connection extends View {
     public float currentX = 70;
     public float currentY = 70;
-    //定义。创建画笔
+    Bitmap clientBitmap;
+    Bitmap serverBitmap;
+    int clientWidth, clientHeight;
+    int serverWidth, serverHeight;
+    int deviceWidth, deviceHeight;
     Paint p = new Paint();
+    private static final String TAG = "Connection";
+
+    private void initialize() {
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        clientBitmap = getBitmap(this.getContext(), R.drawable.ic_client);
+        clientWidth = clientBitmap.getWidth();
+        clientHeight = clientBitmap.getHeight();
+        serverBitmap = getBitmap(this.getContext(), R.drawable.ic_server);
+        serverWidth = serverBitmap.getWidth();
+        serverHeight = serverBitmap.getHeight();
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+
+        deviceWidth = wm.getDefaultDisplay().getWidth();
+        deviceHeight = wm.getDefaultDisplay().getHeight();
+
+    }
+
+    private static Bitmap getBitmap(Context context, int vectorDrawableId) {
+        Bitmap bitmap = null;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            Drawable vectorDrawable = context.getDrawable(vectorDrawableId);
+            bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                    vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            vectorDrawable.draw(canvas);
+        } else {
+            bitmap = BitmapFactory.decodeResource(context.getResources(), vectorDrawableId);
+        }
+        return bitmap;
+    }
 
     public Connection(Context context) {
         super(context);
+        initialize();
     }
 
     public Connection(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initialize();
     }
 
     public Connection(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initialize();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-// TODO Auto-generated method stub
         super.onDraw(canvas);
-//设置画笔的颜色
-        p.setColor(Color.RED);
-//绘制一个小球
-//参数分别是：圆心坐标，半径 ，所使用的画笔
-        canvas.drawCircle(currentX, currentY, 30, p);
+        //绘制中心服务设备
+        canvas.drawBitmap(serverBitmap, (deviceWidth - serverWidth) / 2, (deviceHeight - serverHeight) / 2, null);
+        // 绘制当前触发设备
+        canvas.drawBitmap(clientBitmap, currentX - clientWidth / 2, currentY - clientHeight / 2, null);
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-// TODO Auto-generated method stub
-    //修改当前的坐标
+        //修改当前的坐标
         this.currentX = event.getX();
         this.currentY = event.getY();
-    //重绘小球
+        //重绘
         this.invalidate();
-
         return true;
     }
 }
--- app/src/main/java/t20220049/sw_vision/MainActivity.java
+++ app/src/main/java/t20220049/sw_vision/MainActivity.java
@@ -9,7 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //test change
+        setContentView(R.layout.activity_connection);
+
+
     }
 }
