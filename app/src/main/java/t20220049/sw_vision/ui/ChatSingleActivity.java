18cd8a25d3f97cb9fedca8a4e826b4c64a981866
GY-code <gu_yi163@163.com>
2022-03-30 22:41:27 +0800

    后台摄像完成，需要配置悬浮窗权限

--- app/src/main/java/t20220049/sw_vision/ui/ChatSingleActivity.java
+++ app/src/main/java/t20220049/sw_vision/ui/ChatSingleActivity.java
@@ -2,28 +2,28 @@ package t20220049.sw_vision.ui;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
+import t20220049.sw_vision.CameraService;
 import t20220049.sw_vision.wtc_meeting.IViewCallback;
 import t20220049.sw_vision.wtc_meeting.PeerConnectionHelper;
 import t20220049.sw_vision.wtc_meeting.ProxyVideoSink;
@@ -66,6 +66,11 @@ public class ChatSingleActivity extends AppCompatActivity {
     private TextView photoButton;
     private TextView videoButton;
 
+    private ServiceConnection conn;
+    private CameraService cameraService;
+
+    private static final String TAG = "ChatSingleActivity";
+
     public static void openActivity(Activity activity, boolean videoEnable) {
         Intent intent = new Intent(activity, ChatSingleActivity.class);
         intent.putExtra("videoEnable", videoEnable);
@@ -85,11 +90,40 @@ public class ChatSingleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_collection_terminal);
         initVar();
         initListener();
+        initService();
+
     }
 
 
     private int previewX, previewY;
     private int moveX, moveY;
+    Intent serviceIntent;
+    private void initService(){
+        conn = new ServiceConnection() {
+            /**
+             * 与服务器端交互的接口方法 绑定服务的时候被回调，在这个方法获取绑定Service传递过来的IBinder对象，
+             * 通过这个IBinder对象，实现宿主和Service的交互。
+             */
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                Log.d(TAG, "绑定成功调用：onServiceConnected");
+                // 获取Binder
+                CameraService.LocalBinder binder = (CameraService.LocalBinder) service;
+                cameraService = binder.getService();
+            }
+            /**
+             * 当取消绑定的时候被回调。但正常情况下是不被调用的，它的调用时机是当Service服务被意外销毁时，
+             * 例如内存的资源不足时这个方法才被自动调用。
+             */
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                cameraService=null;
+            }
+        };
+        serviceIntent = new Intent(this, CameraService.class);
+        bindService(serviceIntent, conn, Service.BIND_AUTO_CREATE);
+//        startService(serviceIntent);
+    }
 
     private void initVar() {
         Intent intent = getIntent();
@@ -172,7 +206,15 @@ public class ChatSingleActivity extends AppCompatActivity {
             }
         });
         photoButton.setOnClickListener(v->{
-            havePhoto();
+            if(cameraService!=null){
+                Toast.makeText(getBaseContext(),"拍照",Toast.LENGTH_SHORT).show();
+                cameraService.takePicture();
+            }
+        });
+        videoButton.setOnClickListener(v->{
+            if(cameraService!=null){
+                cameraService.activateRecord();
+            }
         });
     }
     protected void havePhoto(){
@@ -303,6 +345,11 @@ public class ChatSingleActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         disConnect();
+        if(cameraService!=null) {
+            cameraService = null;
+            unbindService(conn);
+//            stopService(serviceIntent);
+        }
         super.onDestroy();
 
     }
--- app/src/main/java/t20220049/sw_vision/utils/PermissionUtil.java
+++ app/src/main/java/t20220049/sw_vision/utils/PermissionUtil.java
@@ -1,9 +1,14 @@
 package t20220049.sw_vision.utils;
 
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -23,9 +28,14 @@ public class PermissionUtil {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             return false;
         }
-        return isNeedRequestPermission(activity, Manifest.permission.CAMERA,
+        return isNeedRequestPermission(activity,
+                Manifest.permission.CAMERA,
                 Manifest.permission.RECORD_AUDIO,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE);
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.FOREGROUND_SERVICE);
+        // Manifest.permission.SYSTEM_ALERT_WINDOW
     }
 
     private static boolean isNeedRequestPermission(Activity activity, String... permissions) {
@@ -36,6 +46,9 @@ public class PermissionUtil {
                 mPermissionListDenied.add(permission);
             }
         }
+//        if(ContextCompat.checkSelfPermission(activity, "Manifest.permission.SYSTEM_ALERT_WINDOW")!=PackageManager.PERMISSION_GRANTED){
+//            startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:t20220049.sw_vision" )));
+//        }
         if (mPermissionListDenied.size() > 0) {
             String[] pears = new String[mPermissionListDenied.size()];
             pears = mPermissionListDenied.toArray(pears);
